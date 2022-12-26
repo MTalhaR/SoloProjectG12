13c4e04ce0c70eda93a74bfcbeeb457f36c779fa
basakalemdag <basakalemdag@gmail.com>
2022-12-26 21:43:56 +0300

    Login Features added

--- src/test/java/steps/PageInitializer.java
+++ src/test/java/steps/PageInitializer.java
@@ -1,12 +1,15 @@
 package steps;
 
+import pages.DashboardPage;
 import pages.LoginPage;
 
 public class PageInitializer {
     public static LoginPage login;
+    public static DashboardPage dashboard;
 
     public static void intializePageObjects(){
         login = new LoginPage();
+        dashboard = new DashboardPage();
 
     }
 }
